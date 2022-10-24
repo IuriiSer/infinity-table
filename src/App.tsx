1e7aef60ce4afa76e02cd45ce11badb014f426d5
IuriiSer <yusergeevi4@gmail.com>
2022-10-24 14:20:30 +0300

    add input to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BookSearch } from './components/BookSearch/BookSearch'
 
 const App: React.FC = () => {
+  const [query, setQuery] = useState<string>('')
+
+  const setQueryHandler = (newQuery: string): void => {
+    setQuery(newQuery)
+  }
+
+  useEffect(() => {
+    console.log('file: App.tsx ~ line 7 ~ query', query)
+  }, [query])
+
   return (
-    <div className="App">
+    <div className='App'>
+      <BookSearch setQueryHandler={setQueryHandler}/>
     </div>
   )
 }
