f4c68336a4e816b58af9f2bfd80016b7eec39997
IuriiSer <yusergeevi4@gmail.com>
2022-10-25 13:46:29 +0300

    little fixes

--- src/components/BookSearch/BookSearch.tsx
+++ src/components/BookSearch/BookSearch.tsx
@@ -6,11 +6,11 @@ interface IBookSearch {
 
 export const BookSearch: React.FC<IBookSearch> = ({ setQueryHandler }: IBookSearch) => {
   return (
-    <div className='row center-align'>
+    <div className='row'>
       <div className='input-field col s12 m6 offset-m3'>
         <i className='material-icons prefix'>book</i>
         <input id='icon_prefix' type='text' className='validate' onChange={(e) => setQueryHandler(e.target.value)}/>
-        <label htmlFor='icon_prefix'>First Name</label>
+        <label htmlFor='icon_prefix'>Search query...</label>
       </div>
     </div>
   )
